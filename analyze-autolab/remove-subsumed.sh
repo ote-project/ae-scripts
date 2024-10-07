72f94b50c133cdd1e75a583f8403de71842a531d
Wen Zhang <zhangwen@cs.berkeley.edu>
2024-10-07 05:54:47 +0000

    Restore autolab remove-subsumed script

--- analyze-autolab/remove-subsumed.sh
+++ analyze-autolab/remove-subsumed.sh
@@ -8,16 +8,16 @@ remove_subsumed() {
     "$HOME/dse/scripts/remove_subsumed.py" "$HOME/dse/app-policies-amended/Autolab" "jdbc:mysql://localhost:3306/autolab_test?allowPublicKeyRetrieval=true&useSSL=false" "autolab_test" "autolab" "12345678"
 }
 
-# for d in $HOME/dse/logs/autolab-*$suffix; do
-#     START=$(date +%s.%N)
-#     cat "$d/annotated-paths/views.sql" | \
-#         $HOME/dse/scripts/analyze-autolab/broaden.sh | \
-#         $HOME/dse/scripts/filter_unsupported_views.sh | \
-#        	remove_subsumed > "$d/annotated-paths/views-minimized.sql"
-#     END=$(date +%s.%N)
-#     DIFF=$(echo "$END - $START" | bc)
-#     echo "$DIFF" > "$d/annotated-paths/remove-subsumed-time-sec.txt"
-# done
+for d in $HOME/dse/logs/autolab-*$suffix; do
+    START=$(date +%s.%N)
+    cat "$d/annotated-paths/views.sql" | \
+        $HOME/dse/scripts/analyze-autolab/broaden.sh | \
+        $HOME/dse/scripts/filter_unsupported_views.sh | \
+        remove_subsumed > "$d/annotated-paths/views-minimized.sql"
+    END=$(date +%s.%N)
+    DIFF=$(echo "$END - $START" | bc)
+    echo "$DIFF" > "$d/annotated-paths/remove-subsumed-time-sec.txt"
+done
 
 policy_dir="$HOME/dse/logs/autolab-$suffix-policy"
 mkdir -p "$policy_dir"
