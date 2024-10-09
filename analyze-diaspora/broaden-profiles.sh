37ddc9c4bd4da016a6c693c20ec635a96c413379
Wen Zhang <zhangwen@cs.berkeley.edu>
2024-10-09 12:08:28 -0700

    Broaden profile, reveal tags

--- analyze-diaspora/broaden-profiles.sh
+++ analyze-diaspora/broaden-profiles.sh
@@ -13,15 +13,20 @@ SCRIPT_DIR=$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )
 SELECT \`id\`, \`diaspora_handle\`, \`first_name\`, \`last_name\`, \`image_url\`, \`image_url_small\`,
        \`image_url_medium\`, \`searchable\`, \`person_id\`, \`created_at\`, \`updated_at\`, \`full_name\`, \`nsfw\`,
        \`public_details\`
-  FROM \`profiles\`;
+FROM \`profiles\`;
 
 SELECT *
-  FROM \`profiles\`
- WHERE \`public_details\` = TRUE;
+FROM \`tags\`, \`taggings\`
+WHERE \`tags\`.\`id\` = \`taggings\`.\`tag_id\`
+  AND \`taggings\`.\`taggable_type\` = 'Profile';
 
 SELECT *
-  FROM \`profiles\`, \`people\`
- WHERE \`profiles\`.\`person_id\` = \`people\`.\`id\`
-   AND \`people\`.\`owner_id\` = _MY_UID;
+FROM \`profiles\`
+WHERE \`public_details\` = TRUE;
+
+SELECT *
+FROM \`profiles\`, \`people\`
+WHERE \`profiles\`.\`person_id\` = \`people\`.\`id\`
+  AND \`people\`.\`owner_id\` = _MY_UID;
 EOF
 ) | remove_subsumed >"$output_policy_file"
