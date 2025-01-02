ce0b3832c4ab3040e24684a0102c32e9c9563a7f
Wen Zhang <zhangwen@cs.berkeley.edu>
2025-01-02 09:03:50 +0000

    odin enforce

---
+++ analyze-theodinproject/enforce.sh
@@ -0,0 +1,5 @@
+#!/usr/bin/env bash
+set -ex
+
+views_path=${1?param missing - views_path.}
+"$HOME/dse/scripts/enforce.py" "$HOME/dse/app-policies-amended/theodinproject" "jdbc:mysql://localhost:3306/theodinproject_test?allowPublicKeyRetrieval=true&useSSL=false" "theodinproject_test" "theodinproject" "12345678" "$views_path"
