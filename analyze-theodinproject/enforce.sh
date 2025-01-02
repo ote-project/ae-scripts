91ec5008746b82c0eeb75eda966f87c53f33beb8
Wen Zhang <zhangwen@cs.berkeley.edu>
2025-01-02 01:09:56 -0800

    odin enforce formatting

--- analyze-theodinproject/enforce.sh
+++ analyze-theodinproject/enforce.sh
@@ -2,4 +2,4 @@
 set -ex
 
 views_path=${1?param missing - views_path.}
-"$HOME/dse/scripts/enforce.py" "$HOME/dse/app-policies-amended/theodinproject" "jdbc:mysql://localhost:3306/theodinproject_test?allowPublicKeyRetrieval=true&useSSL=false" "theodinproject_test" "theodinproject" "12345678" "$views_path"
+"$HOME/dse/scripts/format_views.py" | "$HOME/dse/scripts/enforce.py" "$HOME/dse/app-policies-amended/theodinproject" "jdbc:mysql://localhost:3306/theodinproject_test?allowPublicKeyRetrieval=true&useSSL=false" "theodinproject_test" "theodinproject" "12345678" "$views_path"
